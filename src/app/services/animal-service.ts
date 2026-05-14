81042ffdaaa4fe0f6c71f45261ac8b63ad468dff
Brayan <hola@hotmail.com>
2026-05-14 18:46:25 -0500

    Cambios arreglos

--- src/app/services/animal-service.ts
+++ src/app/services/animal-service.ts
@@ -1,12 +1,13 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimalService {
+
   constructor(private http: HttpClient) { }
   apiUri = '/api/animals';
   httpOptions = new HttpHeaders().set('Content-Type', 'application/json');
@@ -14,9 +15,11 @@ export class AnimalService {
   getAllAnimalsData(): Observable<any> {
     return this.http.get<any>(this.apiUri)
   }
-
-  
-
+  newAnimal(data: any): Observable<any> {
+    return this.http.post<any>(
+      this.apiUri,
+      data,
+      { headers: this.httpOptions });
+  }
 
 }
-
